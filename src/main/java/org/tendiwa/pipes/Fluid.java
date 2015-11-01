6dd129047fb0b27134f18b3975a428bf6ee6bb65
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-02 04:18:48 +0500

    #8 Rename the argument in Fluid#pipe()

--- src/main/java/org/tendiwa/pipes/Fluid.java
+++ src/main/java/org/tendiwa/pipes/Fluid.java
@@ -8,15 +8,19 @@ import java.util.function.Function;
  * @since 0.1.0
  */
 public interface Fluid<I> {
-    default <O> O pipe(final Function<I, O> stage) {
-        return stage.apply((I) this);
+    default <O> O pipe(final Function<I, O> function) {
+        return function.apply((I) this);
+    }
+
+    default <I2> Fluid2<I, I2> with(final I2 another) {
+        return new Fluid2<>((I) this, another);
     }
 
     static <I> Fluid<I> of(final I input) {
         return new Fluid<I>() {
             @Override
-            public <O> O pipe(final Function<I, O> stage) {
-                return stage.apply(input);
+            public <O> O pipe(final Function<I, O> function) {
+                return function.apply(input);
             }
         };
     }
