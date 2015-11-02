1f778f76095246e722a800b5d2ea99b3ccc85057
Georgy Vlasov <wlasowegor@gmail.com>
2015-11-03 00:23:03 +0500

    #15 Rewrite example functions to use the usual Java static functions instead of Function objects

--- src/test/java/org/tendiwa/pipes/RectangleCombinations.java
+++ src/test/java/org/tendiwa/pipes/RectangleCombinations.java
@@ -1,24 +1,16 @@
 package org.tendiwa.pipes;
 
-import java.util.function.BiFunction;
-
 /**
  * @author Georgy Vlasov (suseika@tendiwa.org)
  * @version $Id$
  * @since 0.1.0
  */
 public final class RectangleCombinations {
-    public static BiFunction<Rectangle, Rectangle, Rectangle> bounds =
-        (r1, r2) -> {
-            final int minX = Math.min(r1.x(), r2.x());
-            final int minY = Math.min(r1.y(), r2.y());
-            final int maxX = Math.max(r1.x(), r2.x());
-            final int maxY = Math.max(r1.y(), r2.y());
-            return new Rectangle(
-                minX,
-                minY,
-                maxX - minX + 1,
-                maxY - minY + 1
-            );
-        };
+    public static Rectangle bounds(Rectangle r1, Rectangle r2) {
+        final int x = Math.min(r1.x(), r2.x());
+        final int y = Math.min(r1.y(), r2.y());
+        final int width = Math.max(r1.x(), r2.x()) - x + 1;
+        final int height = Math.max(r1.y(), r2.y()) - y + 1;
+        return new Rectangle(x, y, width, height);
+    }
 }
